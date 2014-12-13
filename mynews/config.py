e5908a2fae256679ab543818efba7dd06ef54f10
junpei0029 <junpei0029@junpei-no-MacBook-Air.local>
2014-12-13 17:27:09 +0900

    mail

--- mynews/config.py
+++ mynews/config.py
@@ -5,7 +5,7 @@ class Config(object):
     CONSUMER_SECRET = '+2vCyqFiehc6MeBBzct4H1/CIss='
     APPID = 'dj0zaiZpPU04TDh6WHptblN1QiZzPWNvbnN1bWVyc2VjcmV0Jng9MDQ-'
     FROM_MAIL_ADDRESS = 'junpei.k.29@gmail.com'
-    FROM_MAIL_PASSWORD = '2108058m'
+    FROM_MAIL_PASSWORD = 'ulxquiclsxjhyfby'
 
 class ProductConfig(Config):
     pass
