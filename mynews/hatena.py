5dec2cfa46656d14e3ff86cb0d6fc8c57588e660
junpei0029 <junpei0029@junpei-no-MacBook-Air.local>
2015-05-08 00:48:40 +0900

    db

--- mynews/hatena.py
+++ mynews/hatena.py
@@ -19,7 +19,8 @@ from MySQLdb.cursors import DictCursor
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-DATABASE_HOST = '153.121.74.93'
+DATABASE_HOST = 'www8079up.sakura.ne.jp'
+#DATABASE_HOST = '153.121.74.93'
 #DATABASE_HOST = 'localhost'
 
 ##################################################################
